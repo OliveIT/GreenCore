b7811b54e10fa71ad34b3b207d3caf63bf82f41d
olivekoko723 <olive.koko723@hotmail.com>
2019-01-10 14:18:55 -0600

    Again migration change

--- database/migrations/2020_01_08_000000_create_geoname_table.php
+++ database/migrations/2020_01_08_000000_create_geoname_table.php
@@ -34,6 +34,6 @@ class CreateGeonameTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('geoname');
+        Schema::dropIfExists('geonames');
     }
 }
